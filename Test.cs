a0bfe818bbfe16eae185cc7fb6995c9d58d4fc26
PsixXxr <psixxxrus@mail.ru>
2025-11-14 11:14:30 +0500

    Update Test.cs

--- Test.cs
+++ Test.cs
@@ -1,29 +1,74 @@
-У меня есть код для NET 5.0+, но я использую C# NET 4.8. Вот код для NET 5.0:
-static void Main(string[] args)
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+class Program
 {
-	var serviceAccountId = "<идентификатор_сервисного_аккаунта>";
-	var keyId = "<идентификатор_открытого_ключа>";
-	var now = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    static void Main(string[] args)
+    {
+        var serviceAccountId = "<идентификатор_сервисного_аккаунта>";
+        var keyId = "<идентификатор_открытого_ключа>";
+        var now = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
 
-	var headers = new Dictionary<string, object>()
-			{
-				{ "kid", keyId }
-			};
+        var headers = new Dictionary<string, object>()
+        {
+            { "kid", keyId }
+        };
 
-	var payload = new Dictionary<string, object>()
-			{
-				{ "aud", "https://iam.api.cloud.yandex.net/iam/v1/tokens" },
-				{ "iss", serviceAccountId },
-				{ "iat", now },
-				{ "exp", now + 3600 }
-			};
+        var payload = new Dictionary<string, object>()
+        {
+            { "aud", "https://iam.api.cloud.yandex.net/iam/v1/tokens" },
+            { "iss", serviceAccountId },
+            { "iat", now },
+            { "exp", now + 3600 }
+        };
 
-	using (var rsa = RSA.Create())
-	{
-		rsa.ImportFromPem(File.ReadAllText("<файл_закрытого_ключа>").ToCharArray());
-		string encodedToken = Jose.JWT.Encode(payload, rsa, JwsAlgorithm.PS256, headers);
-	}
+        string pem = File.ReadAllText("<файл_закрытого_ключа>");
+
+        using (RSA rsa = CreateRsaFromPem(pem))
+        {
+            string encodedToken = Jose.JWT.Encode(payload, rsa, JwsAlgorithm.PS256, headers);
+            Console.WriteLine(encodedToken);
+        }
+    }
+
+    public static RSA CreateRsaFromPem(string pem)
+    {
+        pem = pem.Trim();
+
+        var rsa = RSA.Create();
+
+        if (pem.Contains("BEGIN PRIVATE KEY")) // PKCS#8
+        {
+            byte[] keyData = GetBytesFromPem(pem, "PRIVATE KEY");
+            rsa.ImportPkcs8PrivateKey(keyData, out _);
+        }
+        else if (pem.Contains("BEGIN RSA PRIVATE KEY")) // PKCS#1
+        {
+            byte[] keyData = GetBytesFromPem(pem, "RSA PRIVATE KEY");
+            rsa.ImportRSAPrivateKey(keyData, out _);
+        }
+        else
+        {
+            throw new Exception("Неизвестный формат PEM ключа");
+        }
+
+        return rsa;
+    }
+
+    private static byte[] GetBytesFromPem(string pem, string section)
+    {
+        string header = $"-----BEGIN {section}-----";
+        string footer = $"-----END {section}-----";
+
+        var lines = pem
+            .Replace(header, "")
+            .Replace(footer, "")
+            .Replace("\r", "")
+            .Replace("\n", "");
+
+        return Convert.FromBase64String(lines);
+    }
 }
-
-В NET 4.8 у меня возникает ошибка CS1061: "RSA" не содержит определения "ImportFromPem", и не удалось найти доступный метод расширения "ImportFromPem", принимающий тип "RSA" в качестве первого аргумента (возможно, пропущена директива using или ссылка на сборку) в строке rsa.ImportFromPem(File.ReadAllText("<файл_закрытого_ключа>").ToCharArray());
-Нужно исправить, чтобы я мог использовать этот метод в 4.8
