23b86f233e3351aa26ea2ffc5b4c3896f60c7a83
PsixXxr <psixxxrus@mail.ru>
2025-11-18 15:07:56 +0500

    Update Test.cs

--- Test.cs
+++ Test.cs
@@ -1,1 +1,3 @@
 '{' is invalid after a single JSON value. Expected end of data. Path: $ | LineNumber: 1 | BytePositionInLine: 0.
+
+  https://yandex.cloud/ru/docs/speechkit/stt-v3/api-ref/AsyncRecognizer/getRecognition
